88357cb3442de0ef928c2ea525721f1d901cc648
luckyPaul <luckypaulxu@gmail.com>
2021-11-07 03:31:09 -0500

    week13 commit - kafka cluster

---
+++ week13-kafka-mq/kafka-cluster/src/test/java/cc/paul/demo/paulxukafka/BitkylinKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.paul.demo.paulxukafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulxuKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
